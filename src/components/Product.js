103c3a33992aadfc9da628e7cf1f5831f027acd1
rashik07 <saif67090707@gmail.com>
2022-05-21 22:29:20 +0600

    chooseing

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,30 +1,30 @@
 import React from "react";
 import 'antd/dist/antd.css';
-import { Card, Col, Button  } from 'antd';
+import { Card, Col, Button } from 'antd';
 import {
     ShoppingCartOutlined
-  } from '@ant-design/icons';
+} from '@ant-design/icons';
 const Product = (props) => {
     const { Meta } = Card;
-    const {name,img,price}=props.product;
+    const { name, img, price } = props.product;
 
     return (
         <>
-         
-                <Col span={8}>
-              
+
+            <Col span={8}>
+
                 <Card
-                    
-                    style={{ width: 240,margin:"10px 0px" }}
+
+                    style={{ width: 240, margin: "10px 0px" }}
                     cover={<img alt="example" src={img} />}
-                   
+
                 >
-                    <Meta title={name}  />
+                    <Meta title={name} />
                     <h4>Price:{price}</h4>
-                    <Button type="primary" onClick={()=>props.handleAddToCard(props.product)}>ADD TO CART <ShoppingCartOutlined /></Button>
+                    <Button type="primary" onClick={() => props.handleAddToCard(props.product)}>ADD TO CART <ShoppingCartOutlined /></Button>
                 </Card>
-                </Col>
-               
+            </Col>
+
 
 
 
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import 'antd/dist/antd.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Button,List} from 'antd';
+import { Typography } from 'antd';
 import Product from "./Product";
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
+    const { Text, Link, Paragraph } = Typography;
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
@@ -13,22 +16,42 @@ const Shop = () => {
 
     }, [])
 
-    const handleAddToCard=(product)=>{
+    const handleAddToCard = (product) => {
         console.log(product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+
+    }
+    const randomItem=cart[Math.floor(Math.random()*cart.length)];
+    const handleSelect = () => {
+
+        // const newCart = [...cart, product];
+        console.log(randomItem);
+        const newCart = [ randomItem];
+        setCart(newCart);
+
     }
     return (
         <>
 
             <Row>
                 <Col span={18}>
-                <Row>
-                    {
-                        products.map(product => <Product key={product.id} product={product} handleAddToCard={handleAddToCard}></Product>)
-                    }
+                    <Row>
+                        {
+                            products.map(product => <Product key={product.id} product={product} handleAddToCard={handleAddToCard}></Product>)
+                        }
                     </Row>
                 </Col>
-                <Col span={6}>
-                    this is order summary
+                <Col span={6} style={{ backgroundColor: "#d0d7de", padding: 10 }}>
+                    <Typography.Title level={4} style={{ margin: 0 }}>
+                        Selected Clothes
+                    </Typography.Title>
+
+                    {
+                        cart.map(cardProduct => <List level={5} style={{ paddingTop: "3px", float: "left" }}>{cardProduct.name}</List>)
+                    }
+                    
+                    <Button type="primary" onClick={()=>handleSelect()}>ADD TO CART </Button>
                 </Col>
             </Row>
 
